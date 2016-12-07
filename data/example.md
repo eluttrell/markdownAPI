07985c09893feea24eacc880b2d2b28aaad83ad4
Elijah <elieel@gmail.com>
2016-12-07 09:45:23 -0500

    Complete the filepath exercise

---
+++ data/example.md
@@ -0,0 +1,1 @@
+This text should appear in a file that will be created when I send this request.
--- server.js
+++ server.js
@@ -1,11 +1,50 @@
 const fs = require('fs'),
-      express = require('express'),
-      app = express(),
-      bodyParser = require('body-Parser'),
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-Parser');
 
 app.use(bodyParser.json());
+app.set('view engine', 'hbs');
 
 app.put('/documents/:filepath', function(req, res) {
-    var path = req.params.filepath
-    fs.writeFile()
+    let filepath = './data/' + req.params.filepath;
+    let contents = req.body.contents;
+    fs.writeFile(filepath, contents, (err) => {
+        if (err) {
+            res.status(500);
+            res.json({
+                message: 'Could not save file: ' + err.message
+            });
+        } else {
+            res.json({
+                message: 'File ' + filepath + ' saved.'
+            });
+            console.log('It\'s saved!');
+        }
+    });
+});
+
+app.get('/documents/:filepath', function(req, res) {
+    let filepath = './data/' + req.params.filepath;
+    let contents = req.body.contents;
+    fs.readFile(filepath, 'utf8', (err, data) => {
+      if (err) {
+        res.status(404);
+        res.json({
+          message: 'Requested file not found. ' + err.message
+        });
+      } else {
+        res.json({
+          message: data
+        });
+      }
+    });
+});
+
+app.get('/documents/:filepath/display', function(req, res) {
+
 })
+
+app.listen(1337, function() {
+  console.log('Listening to 1337');
+});
